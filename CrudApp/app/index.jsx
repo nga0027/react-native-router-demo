19ac78750d0295ff2313e73e702a9a4d43b08894
nga0027 <nga0027@gmail.com>
2025-08-12 10:59:07 +0000

    add the add task form

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,16 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style = {styles.addTaskForm}>
+        <TextInput style = {styles.addTaskInput}></TextInput>
+        <Button style = {styles.addTaskButton} title = 'Add'/>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  addTaskForm: {
+    flexDirection: 'row',
+    paddingVertical: 5,
+    paddingHorizontal: 10
+  },
+
+  addTaskInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 5,
+    width: '100%'
+  },
+
+  addTaskButton: {
+    padding: 10
+  }
+})
